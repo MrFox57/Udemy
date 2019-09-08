db1fe3974104562080f5550a654861c581b6a96a
MrFox <iv.elesin@yandex.ru>
2019-09-08 22:40:39 +0300

    это наш первый коммит

---
+++ Lesson1/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(leftBorderWidth);
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+console.log(arr[1]);
+
+// alert('hello world');
+
+// let answer = confirm('Are ypu here?');
+
+// // console.log(answer);
+
+// let answer = prompt('Есть ли вам 18?', "да");
+
+// console.log(typeof(answer));
+
+// console.log("")
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
