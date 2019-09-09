c05b613f6e07e5b8244873a2eaa6e0d764b3d814
MrFox <iv.elesin@yandex.ru>
2019-09-09 22:32:59 +0300

    lesson 3

--- Lesson1/script.js
+++ Lesson1/script.js
@@ -1,31 +1,54 @@
 'use strict';
 
-// var leftBorderWidth = 1;
-// let second = 2;
-// const pi = 3.14;
-// console.log(leftBorderWidth);
+// // var leftBorderWidth = 1;
+// // let second = 2;
+// // const pi = 3.14;
+// // console.log(leftBorderWidth);
 
-let arr = ['plum.png','orange.jpg','apple.bmp'];
+// let arr = ['plum.png','orange.jpg','apple.bmp'];
 
-console.log(arr[1]);
+// console.log(arr[1]);
 
-// alert('hello world');
+// // alert('hello world');
 
-// let answer = confirm('Are ypu here?');
+// // let answer = confirm('Are ypu here?');
 
-// // console.log(answer);
+// // // console.log(answer);
 
-// let answer = prompt('Есть ли вам 18?', "да");
+// // let answer = prompt('Есть ли вам 18?', "да");
 
-// console.log(typeof(answer));
+// // console.log(typeof(answer));
 
-// console.log("")
+// // console.log("")
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-// incr++;
-// decr--;
+// // incr++;
+// // decr--;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i=1; i < 8; i++) {
+//     if (i % 2) {
+//        continue;
+//     }
+//     console.log(i);
+// }
+
+let y = 1; let x = y = 2; console.log(x);
+[ ] + 1 + 2
+console.log([ ] + false - null + true);
--- lesson2/js.js
+++ lesson2/js.js
@@ -1,16 +1,71 @@
-let money = prompt("ваш бюджет на месяц?");
+let money = +prompt("ваш бюджет на месяц?");
 let time = prompt("Введите дату в формате YYYY-MM-DD");
 
 let arr = [money, time];
 // console.log(arr[0], arr[1]);
 
+let appData = {
+    budjet : money, 
+    timeData : time, 
+    expenses : {}, 
+    optionalExpenses : {}, 
+    income : [], 
+    savings : false
+};
 
-let timeData = time;
-let savings = false;
-let expenses = [];
-let optionalExpenses = [];
-let income = [];
-let appData = [money, timeData, expenses, optionalExpenses, income, savings];
 
-alert("бюджет на 1 день " + money/30 + " рублей");
-console.log(appData);
+
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     b = prompt("Во сколько обойдется?", '');
+   
+    // if ( (typeof(a)) ==='string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length<50) {
+    //     console.log("done");
+    //     appData.expenses[a] = b;
+//         } else {
+            
+//         }
+// }
+
+
+
+
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+//     b = prompt("Во сколько обойдется?", '');
+
+//     if ( (typeof(a)) ==='string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length<50) {
+//         console.log("done");
+//         appData.expenses[a] = b;
+//     }
+//     i++;
+// // }
+
+
+
+
+let i = 0;
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    b = prompt("Во сколько обойдется?", '');
+
+    if ( (typeof(a)) ==='string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length<50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    }
+    i++;
+} while (i < 2);
+
+appData.moneyPerDay = appData.budjet / 30;
+alert("бюджет на 1 день " + appData.moneyPerDay + " рублей");
+
+if (appData.moneyPerDay < 100) {
+    console.log ("минимальный уровень доставтка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log ("Середнечок");
+} else if (appData.moneyPerDay > 2000) {
+    console.log ("Богач");
+} else {
+    console.log ("Произошла какая то ошибка");
+}
